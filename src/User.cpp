14c5a6504b2024fa24c735c29f5e9d74f14d4e52
netanelpr <pratn@post.bgu.ac.il>
2019-11-24 13:40:16 +0200

    Update code in LenghtAlgorithem
    Update the code in the LengthAlgoritem.

--- src/User.cpp
+++ src/User.cpp
@@ -38,20 +38,42 @@ User *User::createUser(const std::string &name, const std::string &recommendatio
 }
 //endregion
 
-LengthRecommenderUser::LengthRecommenderUser(const std::string& name): User(name), totalWatchTime(0) {
+LengthRecommenderUser::LengthRecommenderUser(const std::string& name): User(name), totalWatchTime(0), watchedAll(false),
+                                                                        avarage(0), historySize(0), contentNotWatched(){
 }
 
 Watchable *LengthRecommenderUser::getRecommendation(Session &s) {
+    // Create the vector for content that wasnt watch yed
+    if(contentNotWatched.empty() && watchedAll == false){
+        delete(&contentNotWatched);
+        contentNotWatched = s.getContent();
+    }
+
+    unsigned long recIndex = 0;
+    int best;
+
+    if(!contentNotWatched.empty()) {
+        best = avarage - contentNotWatched[0].getLenght();
+        for (int i = 1; i < contentNotWatched.size(); i = i + 1) {
+            if (avarage - contentNotWatched[i].getLenght() < best) {
+                best = avarage - contentNotWatched[i].getLenght();
+                recIndex = i;
+            }
+        }
+
+        return contentNotWatched[recIndex];
+    }
+
     return nullptr;
 }
 
 void LengthRecommenderUser::addToHistory(Watchable *watchable) {
     User::addToHistory(watchable);
+    // TODO remove the watchable from the cotentNotWatched
 
     totalWatchTime = totalWatchTime + watchable->getLength();
-    int avarage = (int)(totalWatchTime / history.size());
-
-
+    avarage = (int)(totalWatchTime / history.size());
+    historySize = historySize + 1;
 }
 
 User *LengthRecommenderUser::createCopy(const std::string &name) const {
